1fe08d146b945ed4e4a01a8f15714a55132c93a1
Krish Naik <krishnaik06@gmail.com>
2023-04-09 17:23:33 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -50,12 +50,3 @@ class DataIngestion:
 
 
 
-## run Data Ingestion
-
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
-
-
---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
+
+
+
+
